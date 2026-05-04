6c84f7eba94391163c9d28df9e8fa8cd1a3ef67c
Tayyab0612 <146617009+Tayyab0612@users.noreply.github.com>
2026-05-04 17:43:41 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,6 +8,15 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=chrome_options)
 
 # Use environment variable - localhost:3001 because docker-compose maps 3001:3000
 APP_URL = os.environ.get('APP_URL', 'http://localhost:3001')
